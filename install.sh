16ae8f556ff213b94860f339a4906105e46f4675
Stig Voss <stig@stigvoss.dk>
2020-08-28 09:34:26 +0200

    Modify install to support both Vim and Neovim

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-mkdir -p ~/.config/nvim/
-
 ln -s $PWD/.tmux.conf ~/.tmux.conf
+
+# Setup Neovim
+mkdir -p ~/.config/nvim/bundle/
 ln -s $PWD/init.nvim ~/.config/nvim/init.nvim
+ln -s $PWD/bundle/Vundle.vim/ ~/.config/nvim/bundle/Vundle.vim/
+
+# Setup Vim
+mkdir -p ~/.vim/bundle/
+ln -s $PWD/bundle/Vundle.vim/ ~/.vim/bundle/Vundle.vim/
