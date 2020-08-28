60504e1149788becc0d718b7fbe00746e8321d1f
Stig Voss <stig@stigvoss.dk>
2020-08-28 09:19:35 +0200

    Add Vundle as submodule.

--- install.sh
+++ install.sh
@@ -2,7 +2,5 @@
 
 mkdir -p ~/.config/nvim/
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
-
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 ln -s $PWD/init.nvim ~/.config/nvim/init.nvim
